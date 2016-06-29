c2cf9fad209713ba6f01eb99980d3a58f72bf6ce
Jane Ruby <janerubyskywalker@gmail.com>
2016-06-29 14:29:59 -0400

    lab completed

---
+++ OOP-Assessment-Lab/starter-code/Dragon.java
@@ -0,0 +1,16 @@
+/**
+ * Created by drewmahrt on 4/27/16.
+ */
+
+//TODO: Make Dragon a subclass of Monster, implement the aboutMe method.
+//Example output for aboutMe: "I am a Dragon with 3 health and do 8 damage!"
+
+public class Dragon extends Monster{
+
+
+    @Override
+    public String aboutMe() {
+             return "I am a Dragon with " + getHealth() + " health and do " + getDamage() + " damage!";
+       }
+
+}
---
+++ OOP-Assessment-Lab/starter-code/Zombie.java
@@ -0,0 +1,14 @@
+/**
+ * Created by drewmahrt on 4/27/16.
+ */
+
+//TODO: Make Skeleton a subclass of monster, and implement the aboutMe method.
+//Example output of the aboutMe method: "I am a Zombie with 4 health and do 10 damage!"
+
+public class Zombie extends Monster{
+
+    @Override
+   public String aboutMe() {
+             return "I am a Zombie with " + getHealth() + " health and do " + getDamage() + " damage!";
+         }
+}
